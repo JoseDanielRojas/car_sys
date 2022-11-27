340c3988ecf51e9ff51450de9c7d2e0d7322a3ad
arcano50 <47094087+arcano50@users.noreply.github.com>
2022-11-27 11:25:23 -0600

    Update cars.js
    Ajuste sobre Post

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const carSchema = require ("../models/cars")
 
-//create car
 router.post("/car",(req,res) => {
     const car = carSchema(req.body);
-    car.save().then((data) => res.json(data))
-    .catch((error)=> res.json)({message:error});
+
+    car.save()
+        .then(
+            data => res.json(data))
+        .catch(
+            error => res.json(error)
+        );
 
 });
 
-
 module.exports = router;
-
